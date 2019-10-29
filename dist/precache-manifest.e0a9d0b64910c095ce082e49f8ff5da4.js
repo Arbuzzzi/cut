8ca30c7f766a225fab498a96ebeea7f3102a9993
Arbuzzzi <42522860+Arbuzzzi@users.noreply.github.com>
2019-10-29 15:31:24 +0300

    init

---
+++ dist/precache-manifest.e0a9d0b64910c095ce082e49f8ff5da4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "8de8ce73176969d4dda6",
+    "url": "css/chunk-vendors.8c625110.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "8de8ce73176969d4dda6",
+    "url": "js/chunk-vendors.f19e5e17.js"
+  },
+  {
+    "revision": "9bdeb2a85524bc1859c3",
+    "url": "css/chunk-185328b9.3f075025.css"
+  },
+  {
+    "revision": "b642f1f08a08cbe3a953",
+    "url": "css/chunk-7a727986.20d0fa8c.css"
+  },
+  {
+    "revision": "b642f1f08a08cbe3a953",
+    "url": "js/chunk-7a727986.51ebc3d6.js"
+  },
+  {
+    "revision": "ef4e6b3cef29759a9acd",
+    "url": "css/chunk-a211ea2c.b1ecc611.css"
+  },
+  {
+    "revision": "ef4e6b3cef29759a9acd",
+    "url": "js/chunk-a211ea2c.794d30f0.js"
+  },
+  {
+    "revision": "d69e31d5800426029f27",
+    "url": "css/chunk-a23b6694.b1ecc611.css"
+  },
+  {
+    "revision": "d69e31d5800426029f27",
+    "url": "js/chunk-a23b6694.52e76e98.js"
+  },
+  {
+    "revision": "99dfb0aa87c15e04632f",
+    "url": "js/app.86e05870.js"
+  },
+  {
+    "revision": "9bdeb2a85524bc1859c3",
+    "url": "js/chunk-185328b9.e7a24648.js"
+  },
+  {
+    "revision": "0726792ade911c92d15286b04be24454",
+    "url": "index.html"
+  },
+  {
+    "revision": "368d5e65d3b83fd8434c3ac33f09a729",
+    "url": "fonts/icomoon.eot"
+  },
+  {
+    "revision": "7b5c250c853e78018d334f3db4d92199",
+    "url": "fonts/icomoon.ttf"
+  },
+  {
+    "revision": "0de7fd1a96659ad8bb81327871d2f2f3",
+    "url": "fonts.css"
+  },
+  {
+    "revision": "11c5a0acb5fd7f4da9bc7389c8705acc",
+    "url": "fonts/icomoon.woff"
+  },
+  {
+    "revision": "3bb6652f36b04a7d52475218052d0650",
+    "url": "img/logo.svg"
+  },
+  {
+    "revision": "c59b33c9993e2100a301b9e735da4de4",
+    "url": "fonts/icomoon.svg"
+  },
+  {
+    "revision": "99dfb0aa87c15e04632f",
+    "url": "css/app.15ecc24f.css"
+  }
+];
